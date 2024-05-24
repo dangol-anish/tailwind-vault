187c499946d1775ef0eeeaf63aad79caf040ff5e
Anish Dangol <dangol.anish001@gmail.com>
2024-05-24 21:59:47 +0545

    Added auth to nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,8 +6,12 @@ import {
   LoginLink,
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { UserNav } from "./UserNav";
 
-export function Navbar() {
+export async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 item-center px-4 md:px-8 mx-auto py-7">
       <div className="md:col-span-3">
@@ -19,12 +23,24 @@ export function Navbar() {
       </div>
       <NavbarLinks />
       <div className="flex items-center gap-x-2 ms-auto md:col-span-3">
-        <Button asChild>
-          <LoginLink>Login</LoginLink>
-        </Button>
-        <Button asChild variant="secondary">
-          <RegisterLink>Register</RegisterLink>
-        </Button>
+        {user ? (
+          <UserNav
+            email={user.email as string}
+            name={user.given_name as string}
+            userImage={
+              user.picture ?? `https://avatar.vercel.sh/${user.given_name}`
+            }
+          />
+        ) : (
+          <div className="flex items-center gap-x-2">
+            <Button asChild>
+              <LoginLink>Login</LoginLink>
+            </Button>
+            <Button asChild variant="secondary">
+              <RegisterLink>Register</RegisterLink>
+            </Button>
+          </div>
+        )}
 
         <div className="md:hidden ">
           <MobileMenu />
