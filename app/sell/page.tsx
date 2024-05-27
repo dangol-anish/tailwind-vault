dc6320e31658f38ed7c1a5022b3a417c2364a27e
Anish Dangol <dangol.anish001@gmail.com>
2024-05-27 11:23:14 +0545

    created select category card

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,3 +1,37 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { SelectCategory } from "../components/SelectCategory";
+
 export default function SellRoute() {
-  return <div>From sell route</div>;
+  return (
+    <section className="max-w-7xl mx-auto px-4 md:px-8">
+      <Card>
+        <form action="">
+          <CardHeader>
+            <CardTitle>Sell your products with ease</CardTitle>
+            <CardDescription>
+              Please describe your product in details!
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-y-10">
+            <div className="flex flex-col gap-y-2">
+              <Label>Name</Label>
+              <Input type="text" placeholder="Name of your Product" />
+            </div>
+            <div className="flex flex-col gap-y-2">
+              <Label>Category</Label>
+              <SelectCategory />
+            </div>
+          </CardContent>
+        </form>
+      </Card>
+    </section>
+  );
 }
